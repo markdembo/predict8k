6757d1f04e6fdc999d51910484a9c76659a54c03
unknown <dembo.mark@gmail.com>
2017-09-01 05:23:29 +0200

    First Luigi test

--- luigi_pipeline.py
+++ luigi_pipeline.py
@@ -1,6 +1,17 @@
-"""Luigi pipeline."""
+"""Luigi pipeline.
+
+To run:
+python -m luigi --module luigi_pipeline Execute --local-scheduler
+
+"""
+import os
+from dotenv import find_dotenv, load_dotenv
 import luigi
-import src.data.step1_get_filings
+import logging
+import logging.config
+import src.data.step1_get_filings as s1_get_filings
+from calendar import monthrange
+import datetime
 
 
 class GetFilings(luigi.Task):
@@ -15,14 +26,48 @@ class GetFilings(luigi.Task):
 
     def output(self):
         """Output of the task."""
-        return luigi.LocalTarget("numbers_up_to_10.txt")
+        load_dotenv(find_dotenv())
+        edgar_dir = os.environ.get("EDGAR_DIR")
+        filename = (
+            "{:filings_%Y-%m.csv}"
+            .format(self.date)
+        )
+        return luigi.LocalTarget(edgar_dir + filename)
 
     def run(self):
         """Task execution."""
-        with self.output().open('w') as f:
-            for a in self.date:
-                f.write("{}\n".format(a))
+        logging.config.fileConfig("logging.conf")
+        logger = logging.getLogger(__name__)
+        load_dotenv(find_dotenv())
+        end_day = monthrange(self.date.year, self.date.month)[1]
+        start_date = datetime.date(self.date.year, self.date.month, 1)
+        end_date = datetime.date(self.date.year, self.date.month, end_day)
+        docs = s1_get_filings.main(
+            start_date.strftime("%Y%m%d"),
+            end_date.strftime("%Y%m%d"),
+            os.environ.get("EDGAR_DIR"),
+            os.environ.get("SUB_INDEX"),
+            os.environ.get("SUB_FILINGS"),
+            self.formtype,
+            logger,
+        )
+        with self.output().open('w') as out_file:
+            docs.to_csv(out_file, encoding="utf-8")
 
 
+class Execute(luigi.WrapperTask):
+    """Class A."""
+
+    date = luigi.parameter.MonthParameter()
+    formtype = luigi.ListParameter(default=["8-K"])
+
+    def requires(self):
+        """Set requirements for the task."""
+        yield [GetFilings(date=self.date, formtype=self.formtype)]
+
+
+"""
 if __name__ == '__main__':
+
     luigi.run()
+"""
--- src/data/step1_get_filings.py
+++ src/data/step1_get_filings.py
@@ -1,46 +1,29 @@
 """Get 8-k filings from edgar database."""
-import os
-import logging
-import logging.config
-from dotenv import find_dotenv, load_dotenv
 from edgarsearch import edgarsearch
-
-SAMPLE_START = "20140601"
-SAMPLE_END = "20141231"
-FILTER_FORMTYPE = ["8-K"]
 
 
 def main(sample_start, sample_end, dir_work, sub_index,
-         sub_filings, filter_formtype):
+         sub_filings, filter_formtype, logger):
     """Download index and filigns from EDGAR database."""
     logger.info('Downloading data from EDGAR database')
-    search = edgarsearch.Search(sample_start,
-                                sample_end,
-                                -1,
-                                dir_work=dir_work,
-                                sub_index=sub_index,
-                                sub_filings=sub_filings,
-                                filter_formtype=filter_formtype)
+    search = edgarsearch.Search(
+        sample_start,
+        sample_end,
+        20,
+        dir_work=dir_work,
+        sub_index=sub_index,
+        sub_filings=sub_filings,
+        filter_formtype=filter_formtype,
+    )
     logger.info('Downloading index files from EDGAR database')
     search.download_index()
     logger.info('Downloading filings from EDGAR database')
-    search.safe_download("months", 1, text_only=True,
-                         fname_form="%Y/%m/%Y%m_%company",
-                         chunk_size=100)
-
-
-if __name__ == "__main__":
-    logging.config.fileConfig("logging.conf")
-    logger = logging.getLogger(__name__)
-
-
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main(SAMPLE_START, SAMPLE_END,
-         os.environ.get("DIR_WORK"),
-         os.environ.get("SUB_INDEX"),
-         os.environ.get("SUB_FILINGS"),
-         FILTER_FORMTYPE)
+    search.download_filings(
+        text_only=True,
+        chunk_size=100,
+        attempts_max=5,
+        timeout=45,
+        fname_form="%Y/%m/%Y%m%d_%company_",
+        show_progress=False,
+    )
+    return search.docs
